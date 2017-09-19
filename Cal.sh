dd42e8011ce1a64da27d6964eabb40cbd62e940e
rajeshpeta <31954603+rajeshpeta@users.noreply.github.com>
2017-09-19 15:46:16 +0530

    Create Cal.sh

---
+++ Cal.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo Please enter a number
+read number
+
+echo What operation would you like to perform: 1: Add, 2: Subtract, 3: Multiple, 4: Divide
+read operation
+
+case $operation in
+    1) math='+';;
+    2) math='-';;
+    3) math='*';;
+    4) math='/';;
+    *) math='not an option, please select again';;
+esac
+echo "$number $math"
