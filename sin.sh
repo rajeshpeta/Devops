cac2022feae5b140fa01ed887d25b2c575a5f0ef
rajeshpeta <31954603+rajeshpeta@users.noreply.github.com>
2017-10-11 09:54:17 +0530

    Update sin.sh

--- sin.sh
+++ sin.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-echo "enter the value of p and t and r"
 p=$1
 t=$2
 r=$3
-i=` expr $p \* $t \* $r `
-i=` expr $i / 100 `
+i=`expr $p \* $t \* $r`
+i=`expr $i / 100`
 echo "intersest is $i"
